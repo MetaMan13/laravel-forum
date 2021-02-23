4d5002cde31166c9c5ca88d4a1254371de5042f3
unknown <imamovica1397@gmail.com>
2021-02-23 19:17:12 +0100

    Responsive Nav #2

--- resources/views/components/profile-dropdown.blade.php
+++ resources/views/components/profile-dropdown.blade.php
@@ -1,9 +1,11 @@
-<div x-data="{open: false}">
+<div x-data="{open: false}" class="relative">
     <img src="/images/user.svg" alt="" class="bg-gray-500 p-0.5 rounded-full self-center" @click="open = true">
 
-    <div x-show="open" @click.away="open = false">
-        <a href="#">Profile</a>
-        <a href="#">Settings</a>
-        <a href="#">Logout</a>
+    <div x-show="open" @click.away="open = false" class="absolute z-10 top-0 right-0 mt-10 bg-gray-100 shadow-md rounded-md text-left border border-gray-200 w-32">
+        <a href="/profile" class=" font-medium text-md block px-4 py-2 hover:text-gray-800 hover:bg-white rounded-t-md">Profile</a>
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();this.closest('form').submit();">{{ __('Log out') }}</x-responsive-nav-link>
+        </form>
     </div>
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,4 @@
 @section('content')
     @include('page-parts.navigation')
     <h1>Hewo {{ auth()->user()->name }}</h1>
-
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();this.closest('form').submit();">{{ __('Log out') }}</x-responsive-nav-link>
-    </form>
 @endsection
