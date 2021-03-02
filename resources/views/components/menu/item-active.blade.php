141eb6cee654935de7b5762fc6e6a2f5e65f2392
unknown <imamovica1397@gmail.com>
2021-03-02 18:28:44 +0100

    UI #4, basic layout, main menu + components, darkmode

---
+++ resources/views/components/menu/item-active.blade.php
@@ -0,0 +1,3 @@
+<a href="/" class="flex py-2 px-4 font-semibold text-md border-l-4  bg-white text-blue-600 border-blue-600 dark:border-blue-300 dark:text-blue-300 dark:bg-gray-700">
+    {{ $slot }}
+</a>
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -4,26 +4,37 @@
     @include('page-parts.navigation')
     <div class="pt-16 min-h-screen flex">
 
-        <div class="min-h-full w-3/12 -mt-1">
-            <div class="w-full h-full bg-gray-50 pl-4 flex justify-between md:pl-6 lg:pl-8 xl:pl-12 2xl:pl-16 border-r-2 border-gray-100 dark:bg-gray-900 dark:border-gray-800 delay-200 transition-all duration-300 ease-in-out">
-                <h1>HEWO</h1>
+        <div class="min-h-full w-3/12 -mt-1 text-gray-600">
+            <div class="w-full h-full bg-gray-50 pl-4 flex justify-between md:pl-8 lg:pl-16 xl:pl-24 2xl:pl-32 border-r-2 border-gray-100 dark:bg-gray-900 dark:border-gray-800">
+                <ul class="w-full pt-10">
+                    <li class="mb-2">
+                        <x-menu.item>
+                            <x-icons.home></x-icons.home>
+                            Home
+                        </x-menu.item>
+                    </li>
+                    <li class="mb-2">
+                        <x-menu.item-active>
+                            <x-icons.compas></x-icons.compas>
+                            Posts
+                        </x-menu.item-active>
+                    </li>
+                    <li>
+                        <x-menu.item>
+                            <x-icons.users></x-icons.users>
+                            People
+                        </x-menu.item>
+                    </li>
+                </ul>
             </div>
         </div>
-
-        <div class="min-h-full w-6/12 bg-gray-100 -mt-1 dark:bg-gray-800 delay-200 transition-all duration-300 ease-in-out"></div>
-
+        
+        <div class="min-h-full w-6/12 bg-gray-100 -mt-1 dark:bg-gray-800"></div>
+        
         <div class="min-h-full w-3/12 -mt-1">
-            <div class="w-full h-full bg-gray-50 pr-4 md:pr-6 lg:pr-8 xl:pr-12 2xl:pr-16 text-right border-l-2 border-gray-100 dark:bg-gray-900 dark:border-gray-800  delay-200 transition-all duration-300 ease-in-out">
-                <h1>Hewwo</h1>
+            <div class="w-full h-full bg-gray-50 pr-4 md:pr-8 lg:pr-16 xl:pr-24 2xl:pr-32 text-right border-l-2 border-gray-100 dark:bg-gray-900 dark:border-gray-800">
+                <div class="bg-gray-300 w-full h-full"></div>
             </div>
         </div>
-        {{-- <h1>Current number  of posts {{ count($posts) }}</h1>
-        <a href="/post/create">Create a new post</a>
-        @foreach ($posts as $post)
-            <div class="border mb-10">
-                <a href="/post/{{ $post->id }}">{{ $post->title }}</a>
-                <h3>{{ $post->body }}</h3>
-            </div>
-        @endforeach --}}
     </div>
 @endsection
