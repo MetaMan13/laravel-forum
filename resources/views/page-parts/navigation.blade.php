ce910cd574498939bc7e549eb136724f9ce830f1
unknown <imamovica1397@gmail.com>
2021-03-02 01:48:09 +0100

    UI #3

--- resources/views/page-parts/navigation.blade.php
+++ resources/views/page-parts/navigation.blade.php
@@ -1,9 +1,9 @@
-<div class="bg-gray-50 w-full fixed top-0 left-0 border-b-2 border-gray-100 dark:bg-gray-900 dark:border-gray-800">
+<div class="bg-gray-50 w-full fixed top-0 left-0 border-b-2 border-gray-100 dark:bg-gray-900 dark:border-gray-800 delay-200 transition-all duration-300 ease-in-out">
     <div class="mx-auto px-4 py-4 flex justify-between md:px-6 lg:px-8 xl:px-12 2xl:px-16">
         <div class="flex">
             <a href="/" class="flex">
                 <x-icons.logo></x-icons.logo>
-                <h1 class="ml-1 self-center font-semibold text-blue-600 text-lg dark:text-blue-300">Justdev</h1>
+                <h1 class="ml-1 self-center font-semibold text-blue-600 text-lg dark:text-blue-300 delay-300 transition-all duration-200 ease-in-out">Justdev</h1>
             </a>
         </div>
 
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -5,15 +5,15 @@
     <div class="pt-16 min-h-screen flex">
 
         <div class="min-h-full w-3/12 -mt-1">
-            <div class="w-full h-full bg-gray-50 pl-4 flex justify-between md:pl-6 lg:pl-8 xl:pl-12 2xl:pl-16 border-r-2 border-gray-100 dark:bg-gray-900 dark:border-gray-800">
+            <div class="w-full h-full bg-gray-50 pl-4 flex justify-between md:pl-6 lg:pl-8 xl:pl-12 2xl:pl-16 border-r-2 border-gray-100 dark:bg-gray-900 dark:border-gray-800 delay-200 transition-all duration-300 ease-in-out">
                 <h1>HEWO</h1>
             </div>
         </div>
 
-        <div class="min-h-full w-6/12 bg-gray-100 -mt-1"></div>
+        <div class="min-h-full w-6/12 bg-gray-100 -mt-1 dark:bg-gray-800 delay-200 transition-all duration-300 ease-in-out"></div>
 
         <div class="min-h-full w-3/12 -mt-1">
-            <div class="w-full h-full bg-gray-50 pr-4 md:pr-6 lg:pr-8 xl:pr-12 2xl:pr-16 text-right border-l-2 border-gray-100 dark:bg-gray-900 dark:border-gray-800">
+            <div class="w-full h-full bg-gray-50 pr-4 md:pr-6 lg:pr-8 xl:pr-12 2xl:pr-16 text-right border-l-2 border-gray-100 dark:bg-gray-900 dark:border-gray-800  delay-200 transition-all duration-300 ease-in-out">
                 <h1>Hewwo</h1>
             </div>
         </div>
