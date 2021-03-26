df26dff0c093db87712464e84a665a1bae7a2605
unknown <imamovica1397@gmail.com>
2021-03-26 03:11:54 +0100

    Profile Index update

--- README.md
+++ README.md
@@ -3,28 +3,4 @@ border-gray-200 dark:border-gray-700
 bg-gray-50 ( normal ) dark: bg-gray-800
 bg-white ( hover, focus, etc states )
 
-
-projects
-    id - integer
-    name - string
-
-environments
-    id - integer
-    project_id - integer
-    name - string
-
-deployments
-    id - integer
-    environment_id - integer
-    commit_hash - string
-
-
-user
-    id
-
-group
-    id
-
-group_user
-    group_id
-    user_id
+User profile
--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -4,118 +4,59 @@
 
     @include('page-parts.navigation')
 
-    <div class="pt-4 min-h-screen flex md:pt-16">
-        <x-menu.layout>
-            <x-menu.item href="/">
-                <x-icons.home></x-icons.home>
-                Home
-            </x-menu.item>
-            <x-menu.item href="/post">
-                <x-icons.compas></x-icons.compas>
-                Posts
-            </x-menu.item>
-            <x-menu.item href="/profile">
-                <x-icons.users></x-icons.users>
-                People
-            </x-menu.item>
-            <x-menu.item href="/country">
-                <x-icons.flag></x-icons.flag>
-                Countries
-            </x-menu.item>
-            <x-menu.item href="/group">
-                <x-icons.group></x-icons.group>
-                Groups
-            </x-menu.item>
-        </x-menu.layout>
-        
-        <x-main-content.layout>
+    <div class="bg-red-300 min-h-screen w-full pt-20 mx-auto px-4 py-4 flex justify-between md:px-8 lg:px-16 xl:px-24 2xl:px-32 lg:bg-yellow-300">
+        <div class="min-h-full w-full bg-yellow-300 md:grid md:grid-cols-12">
 
-            <div class="flex w-full mt-12 md:mt-0">
-                <div class="self-center">
-                    <img src="{{ $user->image }}" alt="" class="h-16 rounded-full">
-                </div>
-                    
-                <div class="ml-4 self-center">
+            <div class="bg-blue-600 hidden md:block md:min-h-full md:col-span-4 lg:col-span-3 xl:col-span-2 px-4 pt-4">
+
+                <div class="bg-blue-500 flex flex-col content-center items-center">
+
                     <div>
-                        <h3 class="text-md font-bold">{{ $user->nickname }}</h3>
+                        <img src="{{ $user->image }}" alt="" class="h-40 rounded-full">
                     </div>
 
-                    <div class="flex mt-1 text-sm">
-                        <div>
-                            <p>
-                                <span class="font-semibold">
-                                    {{ count($user->follows) }}
-                                </span>
+                    <div class="mt-2">
+                        <h3 class="text-xl font-semibold">{{ $user->nickname }}</h3>
+                    </div>
 
-                                following
-                            </p>
+                </div>
+
+                <div class="bg-blue-700 mt-4">
+                    <div>
+
+                        <div class="flex">
+                             
+                            <div class="flex-shrink-0 self-start">
+                                <img src="{{ $user->country->image }}" alt="" class="h-8">
+                            </div>
+
+                            <div class="w-full ml-2 self-center">
+                                <h4> {{ $user->country->name }} </h4>
+                            </div>
                         </div>
 
-                        <div class="ml-2">
-                            <p>
-                                <span class="font-semibold">
-                                    {{ count($user->followers) }}
-                                </span>
-
-                                followers
-                            </p>
-                        </div>
                     </div>
                 </div>
-            </div>
 
-            @if (auth()->user()->id === $user->id)
-                <a href="/profile/{{ $user->nickname }}/edit" class="mt-4 rounded-md py-1.5 text-sm block text-center font-semibold uppercase bg-blue-600 text-gray-50 border-2 border-gray-200 dark:bg-blue-300 dark:border-gray-700 dark:text-gray-800">Edit profile</a>
-            @endif
+                <div class="bg-blue-800 mt-4">
 
-            @if (auth()->user()->id !== $user->id)
-                @if (auth()->user()->follows->contains('follow_id', $user->id))
-                    <div class="block w-full">
-                        <form action="/unfollow" method="POST" class="self-center">
-                            @csrf
-                            @method('POST')
-                                <input type="hidden" value="{{ $user->id }}" name="user_id">
-                                <button type="submit" class="w-full mt-4 rounded-md py-1.5 text-sm block text-center font-semibold uppercase bg-blue-600 text-gray-50 border-2 border-gray-200 dark:bg-blue-300 dark:border-gray-700 dark:text-gray-800">Unfollow</button>
-                        </form>
+                    <div>
+                        <h4>Timeline</h4>
                     </div>
-                @else
-                    <div class="block w-full">
-                        <form action="/follow" method="POST" class="self-center">
-                            @csrf
-                            @method('POST')
-                            <input type="hidden" value="{{ $user->id }}" name="user_id">
-                            <button type="submit" class="w-full mt-4 rounded-md py-1.5 text-sm block text-center font-semibold uppercase bg-blue-600 text-gray-50 border-2 border-gray-200 dark:bg-blue-300 dark:border-gray-700 dark:text-gray-800">Follow</button>
-                        </form>
+
+                    <div>
+                        <h4>General information</h4>
                     </div>
-                @endif
-            @endif
 
-            <div class="mt-8 mb-12">
-                @foreach ($user->posts as $post)
-                    <div class="border-2 border-gray-200 dark:border-gray-700 mb-8 py-2 px-2 shadow-sm md:py-4 md:px-4">
-                        <div>
-                            <a href="/post/{{ $post->id }}" class="text-md font-semibold">
-                                {{ $post->title }}
-                            </a>
-                        </div>
-
-                        <div class="mt-1">
-                            <p class="text-sm">
-                                {{ now()->diffForHumans($post->created_at, true) }} ago
-                            </p>
-                        </div>
+                    <div>
+                        <h4>Edit information</h4>
                     </div>
-                @endforeach
-            </div>
 
-        </x-main-content.layout>
-        
-        <div class="hidden md:block md:min-h-full md:w-3/12 md:-mt-1 fixed right-0">
-            <div class="w-full h-screen bg-gray-50 pr-4 md:pr-8 lg:pr-16 xl:pr-24 2xl:pr-32 text-right border-l border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-                <div class="w-full h-full">
-                    <h3>HEWO</h3>
                 </div>
+
             </div>
+
+            <div class="bg-gray-50  hidden md:block md:min-h-full md:col-span-8 lg:col-span-9 xl:col-span-10"></div>
         </div>
     </div>
 
