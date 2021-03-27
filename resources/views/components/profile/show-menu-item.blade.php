bd19a4a9039b9c141b6362a0c83f94db2d5c6489
unknown <imamovica1397@gmail.com>
2021-03-27 13:56:33 +0100

    Profile Show UI update #3

--- resources/views/components/profile/show-menu-item.blade.php
+++ resources/views/components/profile/show-menu-item.blade.php
@@ -16,7 +16,7 @@
             {{ $text }}
         </a>
     @else
-        <a href="{{ $href }}" class="flex rounded-md text-base py-2 px-4 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 group hover:border-gray-400 dark:hover:border-gray-500">
+        <a href="{{ $href }}" class="flex rounded-md text-base font-medium py-2 px-4 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 group hover:text-gray-800 dark:hover:text-gray-100">
 
             <div class="self-center mr-2 group-hover:text-blue-600 dark:group-hover:text-blue-300">
                 {{ $slot }}
--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -5,7 +5,7 @@
     @include('page-parts.navigation')
 
     <div class="min-h-screen w-full pt-20 mx-auto px-4 py-4 flex justify-between md:px-8 lg:px-16 xl:px-24 2xl:px-32">
-        <div class="min-h-full w-full md:grid md:grid-cols-12 border-2 border-gray-200 dark:border-gray-700 -mt-5">
+        <div class="min-h-full w-full md:grid md:grid-cols-12 border-2 border-gray-200 dark:border-gray-700 -mt-5 -mb-4">
 
             <div class="bg-gray-50  dark:bg-gray-800 border-r-2 border-gray-200 dark:border-gray-700 hidden md:block md:min-h-full md:col-span-4 lg:col-span-3 xl:col-span-2 px-4 pt-4">
 
@@ -64,11 +64,11 @@
                     </x-profile.show-menu-item>
                     
                     <x-profile.show-menu-item href="#" text="Edit profile">
-                        <x-icons.timeline></x-icons.timeline>
+                        <x-icons.edit></x-icons.edit>
                     </x-profile.show-menu-item>
                     
                     <x-profile.show-menu-item href="#" text="Security">
-                        <x-icons.timeline></x-icons.timeline>
+                        <x-icons.lock></x-icons.lock>
                     </x-profile.show-menu-item>
 
                 </div>
