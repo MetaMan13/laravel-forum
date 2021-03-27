8d069e79d09efdf92f5925e2e195360d38b28df6
unknown <imamovica1397@gmail.com>
2021-03-27 12:19:28 +0100

    Profile show UI update

---
+++ resources/views/components/profile/show-menu-item.blade.php
@@ -0,0 +1,3 @@
+<div class="mb-4">
+    <a href="#" class="rounded-md text-base block py-2 px-4 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 hover:bg-blue-600 hover:text-white dark:hover:bg-blue-300 dark:hover:text-gray-900">{{ $slot }}</a>
+</div>
--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -4,10 +4,10 @@
 
     @include('page-parts.navigation')
 
-    <div class="bg-red-300 min-h-screen w-full pt-20 mx-auto px-4 py-4 flex justify-between md:px-8 lg:px-16 xl:px-24 2xl:px-32 lg:bg-gray-50">
-        <div class="min-h-full w-full bg-yellow-600 md:grid md:grid-cols-12 border-2 border-gray-200 -mt-5">
+    <div class="min-h-screen w-full pt-20 mx-auto px-4 py-4 flex justify-between md:px-8 lg:px-16 xl:px-24 2xl:px-32">
+        <div class="min-h-full w-full md:grid md:grid-cols-12 border-2 border-gray-200 dark:border-gray-700 -mt-5">
 
-            <div class="bg-gray-50 border-r-2 border-gray-200 dark:border-gray-700 hidden md:block md:min-h-full md:col-span-4 lg:col-span-3 xl:col-span-2 px-4 pt-4">
+            <div class="bg-gray-50  dark:bg-gray-800 border-r-2 border-gray-200 dark:border-gray-700 hidden md:block md:min-h-full md:col-span-4 lg:col-span-3 xl:col-span-2 px-4 pt-4">
 
                 <div class="flex flex-col content-center items-center mt-2 border-b-2 border-gray-200 dark:border-gray-700">
 
@@ -16,7 +16,7 @@
                     </div>
 
                     <div class="mt-2 mb-3">
-                        <h3 class="text-2xl font-bold">{{ $user->nickname }}</h3>
+                        <h3 class="text-2xl font-medium">{{ $user->nickname }}</h3>
                     </div>
 
                 </div>
@@ -24,7 +24,7 @@
                 <div class="mt-6">
 
                     <div>
-                        <h3 class="font-semibold text-lg">Country:</h3>
+                        <h3 class="font-medium text-lg">Country:</h3>
                     </div>
 
                     <div class="flex mt-2">         
@@ -33,48 +33,49 @@
                         </div>
 
                         <div class="w-full ml-2 self-center">
-                            <h4 class="text-md">{{ $user->country->name }}</h4>
+                            <h4>{{ $user->country->name }}</h4>
                         </div>
                     </div>
 
                 </div>
 
+                @if (count($user->groups) != 0)
+                    <div class="mt-6">
+                        <div>
+                            <h3 class="font-medium text-lg">Groups:</h3>
+                        </div>
+
+                        <div class="mt-2">
+                            @foreach ($user->groups as $group)
+                                <div class="mr-3">
+                                    <h3>
+                                        {{ $group->name }}
+                                    </h3>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                @endif
 
                 <div class="mt-6">
-                    <div>
-                        <h3 class="font-semibold text-lg">Groups:</h3>
-                    </div>
 
-                    <div class="mt-2">
-                        @foreach ($user->groups as $group)
-                            <div class="mr-3">
-                                <h3 class="text-md">
-                                    {{ $group->name }}
-                                </h3>
-                            </div>
-                        @endforeach
-                    </div>
-                </div>
+                    <x-profile.show-menu-item-active>
+                        Timeline
+                    </x-profile.show-menu-item-active>
 
-                <div class="mt-6">
+                    <x-profile.show-menu-item>
+                        Edit profile
+                    </x-profile.show-menu-item>
 
-                    <div class="bg-white py-2 px-4 mb-4 border border-gray-300">
-                        <h4>Timeline</h4>
-                    </div>
-
-                    <div class="bg-white py-2 px-4 mb-4 border border-gray-300">
-                        <h4>General information</h4>
-                    </div>
-
-                    <div class="bg-white py-2 px-4 border border-gray-300">
-                        <h4>Edit information</h4>
-                    </div>
+                    <x-profile.show-menu-item>
+                        Security
+                    </x-profile.show-menu-item>
 
                 </div>
 
             </div>
 
-            <div class="bg-gray-50 hidden md:block md:min-h-full md:col-span-8 lg:col-span-9 xl:col-span-10"></div>
+            <div class="bg-gray-50  dark:bg-gray-800 hidden md:block md:min-h-full md:col-span-8 lg:col-span-9 xl:col-span-10"></div>
         </div>
     </div>
 
